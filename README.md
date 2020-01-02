78aaa505edeaa86ea7b7a9971bd9744a4bc55d4a
chenzipeng10 <chenzipeng10@126.com>
2020-01-02 23:56:39 +0800

    gmall-user

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # GMall
 商城
+
+## gmall-user 用户服务
+端口号：8080
+主流框架必有的包：controller，service，mapper，bean；mapper需要配置对应的xml映射文件(*Mapper.xml)。
---
+++ gmall-user/src/main/java/com/example/gmall/user/bean/UmsMember.java
@@ -0,0 +1,23 @@
+package com.example.gmall.user.bean;
+
+public class UmsMember {
+    private String id;
+    private String member_level_id;
+    private String username;
+    private String password;
+    private String nickname;
+    private String phone;
+    private String status;
+    private String create_time;
+    private String icon;
+    private String gender;
+    private String birthday;
+    private String city;
+    private String job;
+    private String personalized_signature;
+    private String source_type;
+    private String integration;
+    private String growth;
+    private String luckey_count;
+    private String history_integration;
+}
