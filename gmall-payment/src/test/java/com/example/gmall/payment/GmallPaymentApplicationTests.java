fda3e8aa17a05682650c58349407b4590856f823
chenzipeng10 <chenzipeng10@126.com>
2020-02-01 22:13:52 +0800

    加入ActiveMQ消息中间件的支持

--- gmall-payment/src/test/java/com/example/gmall/payment/GmallPaymentApplicationTests.java
+++ gmall-payment/src/test/java/com/example/gmall/payment/GmallPaymentApplicationTests.java
@@ -1,13 +1,29 @@
 package com.example.gmall.payment;
 
+import com.example.gmall.mq.ActiveMQUtil;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
 
 @SpringBootTest
 public class GmallPaymentApplicationTests {
 
+    @Autowired
+    ActiveMQUtil activeMQUtil;
+
     @Test
-    public void contextLoads() {
+    public void contextLoads() throws JMSException {
+
+        ConnectionFactory connectionFactory = activeMQUtil.getConnectionFactory();
+
+        Connection connection = connectionFactory.createConnection();
+
+        System.out.println(connection);
+
     }
 
 }
