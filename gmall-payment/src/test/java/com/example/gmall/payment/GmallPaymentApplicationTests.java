48c7e74b78bc81121aad363f63a5bc21a637ea6b
chenzipeng10 <chenzipeng10@126.com>
2020-02-01 23:10:22 +0800

    解决ActiveMQ连接问题

--- gmall-payment/src/test/java/com/example/gmall/payment/GmallPaymentApplicationTests.java
+++ gmall-payment/src/test/java/com/example/gmall/payment/GmallPaymentApplicationTests.java
@@ -2,13 +2,14 @@ package com.example.gmall.payment;
 
 import com.example.gmall.mq.ActiveMQUtil;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.jms.Connection;
-import javax.jms.ConnectionFactory;
-import javax.jms.JMSException;
+import javax.jms.*;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class GmallPaymentApplicationTests {
 
--- gmall-service-util/src/main/java/com/example/gmall/mq/ActiveMQUtil.java
+++ gmall-service-util/src/main/java/com/example/gmall/mq/ActiveMQUtil.java
@@ -2,16 +2,17 @@ package com.example.gmall.mq;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.pool.PooledConnectionFactory;
+import org.springframework.context.annotation.Bean;
 
 import javax.jms.ConnectionFactory;
 
 public class ActiveMQUtil {
-    PooledConnectionFactory pooledConnectionFactory=null;
+    PooledConnectionFactory pooledConnectionFactory = null;
 
     public ConnectionFactory init(String brokerUrl) {
         ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory(brokerUrl);
-         //加入连接池
-        pooledConnectionFactory=new PooledConnectionFactory(factory);
+        //加入连接池
+        pooledConnectionFactory = new PooledConnectionFactory(factory);
         //出现异常时重新连接
         pooledConnectionFactory.setReconnectOnException(true);
         //
@@ -20,7 +21,7 @@ public class ActiveMQUtil {
         return pooledConnectionFactory;
     }
 
-    public ConnectionFactory getConnectionFactory(){
+    public ConnectionFactory getConnectionFactory() {
         return pooledConnectionFactory;
     }
 }
