f940e1ade16e6611988bb5b9b5135eed63b257d9
chenzipeng10 <chenzipeng10@126.com>
2020-02-03 16:22:53 +0800

    修改库存管理系统，使其正常运行

---
+++ gware-manage/src/main/java/com/example/gware/mapper/WareOrderTaskDetailMapper.java
@@ -0,0 +1,12 @@
+package com.example.gware.mapper;
+
+import com.example.gware.bean.WareOrderTaskDetail;
+import tk.mybatis.mapper.common.Mapper;
+
+
+/**
+ * @param
+ * @return
+ */
+public interface WareOrderTaskDetailMapper extends Mapper<WareOrderTaskDetail> {
+}
---
+++ gware-manage/src/main/java/com/example/gware/mapper/WareOrderTaskMapper.java
@@ -0,0 +1,12 @@
+package com.example.gware.mapper;
+
+import com.example.gware.bean.WareOrderTask;
+import tk.mybatis.mapper.common.Mapper;
+
+
+/**
+ * @param
+ * @return
+ */
+public interface WareOrderTaskMapper extends Mapper<WareOrderTask> {
+}
